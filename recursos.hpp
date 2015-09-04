0e73139782e15050a51de3b1f46b2f8201be7e9c
Winsu <Winsu@users.noreply.github.com>
2015-09-04 20:40:18 +0200

    Create recursos.hpp

---
+++ recursos.hpp
@@ -0,0 +1,36 @@
+#ifndef RECURSOS_HPP
+#define RECURSOS_HPP
+
+#include<string>
+
+
+
+const std::string file_name_cubierta_G("cubierta-G-3.1.txt");
+const std::string file_name_Ejemplos_identidad_Hi("ejemplo-identidad.txt");
+const std::string file_name_Ejemplos_identidad_Ji("ejemplo-identidad-2.txt");
+const std::string file_name_Ejemplos_identidad_Ki("ejemplo-identidad-3.txt");
+const std::string file_name_Ejemplos_contenciones_H("ejemplo-contenciones.txt");
+const std::string file_name_Ejemplos_contenciones_J("ejemplo-contenciones-2.txt");
+const std::string file_name_Ejemplos_contenciones_K("ejemplo-contenciones-3.txt");
+const std::string file_name_cube_p("cubo-p-3.1.txt");
+const std::string file_name_cube_x("cubo-x-3.1.txt");
+const std::string file_name_ejemplo_MV_5("ejemplo-MV-5.txt");
+/*
+const std::string file_path_variable("C:\\");
+const std::string file_path_final("Users\\DANI\\Presentacion\\src\\");
+*/
+
+const std::string file_path_variable("C:\\Users\\DANI\\Downloads");
+const std::string file_path_final("\\Presentacion-2013_07_26_17_57\\Presentacion\\src\\");
+
+const std::string file_name_full_Cover_G	=	file_path_variable+file_path_final+file_name_cubierta_G;
+const std::string file_name_full_Cube_p		=	file_path_variable+file_path_final+file_name_cube_p;
+const std::string file_name_full_Cube_x		=	file_path_variable+file_path_final+file_name_cube_x;
+const std::string file_name_full_Cover_H	=	file_path_variable+file_path_final+file_name_Ejemplos_contenciones_H;
+const std::string file_name_full_Cover_J	=	file_path_variable+file_path_final+file_name_Ejemplos_contenciones_J;
+const std::string file_name_full_Cover_K	=	file_path_variable+file_path_final+file_name_Ejemplos_contenciones_K;
+const std::string identity_Cover_Hi			=	file_path_variable+file_path_final+file_name_Ejemplos_identidad_Hi;
+const std::string identity_Cover_Ji			=	file_path_variable+file_path_final+file_name_Ejemplos_identidad_Ji;
+const std::string identity_Cover_Ki			=	file_path_variable+file_path_final+file_name_Ejemplos_identidad_Ki;
+
+#endif //RECURSOS_HPP
